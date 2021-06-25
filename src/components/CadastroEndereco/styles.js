1836d8d5a4333693c2bccd718733b190e1034100
ingridbromer <ingridb@id.uff.br>
2021-06-25 08:47:59 -0300

    Estilização CadastroEndereco, UserForm e Cadastro

--- src/components/CadastroEndereco/styles.js
+++ src/components/CadastroEndereco/styles.js
@@ -1,24 +1,59 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+   
+`;
 export const Input = styled.input`
     margin: 1.5rem 2rem;
-    height: 10%;
+    width: 80%;
+    height: 14%;
     display: flex;
     justify-content: flex-start;
     align-items: center;
     border-color: cadetblue;
+    background-color: #A0A0A0
     
 `;
 
 export const AreaForm2 = styled.div`
-    width: 40%;
+    width: 80%;
     align-items: center;
 `;
 
 export const Form= styled.form`
-    height: 100%;
-    width: 80%;
     justify-content: space-between;
     display: flex;
     text-align: center;
 
+`;
+
+
+export const ContainerTitle = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+`;
+
+export const Title = styled.h1`
+    display: flex;
+    align-items: center;
+    font-size: 32px;
+    font-weight: bold;
+    margin-top: 2rem;
+    color: #242528;
+`;
+
+export const Button = styled.button`
+    width: 30%;
+    height: 15%;
+    border-radius: 0.5em;
+    margin-left: 15%;
 `;
