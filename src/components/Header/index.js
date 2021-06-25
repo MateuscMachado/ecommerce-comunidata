24c483052e3a23f0b15b1560ac6dc62c7a430a10
Cadu7 <cadumdecarcalho@gmail.com>
2021-06-25 10:10:23 -0300

    Link para a home no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import { HeaderStyle, Input, LogoHeader, Div, ButtonPesquisa, Entrar, Text } from "./styles";
+import { HeaderStyle, Input, LogoHeader, Formatacao, Div, ButtonPesquisa, Entrar, Text } from "./styles";
 import { Link } from "react-router-dom";
 import { api } from "../../services/api";
 import { useState } from "react";
@@ -7,7 +7,7 @@ function Header() {
     const [prodName, setProdName] = useState();
     console.log(prodName);
 
-    const buscarNome = async () =>{
+    const buscarNome = async () => {
 
         var response = await api.get(`/produtos/nome?nome=${document.getElementById("search").value}`)
         setProdName(response.data)
@@ -15,18 +15,20 @@ function Header() {
 
     return (
         <HeaderStyle>
-            <LogoHeader  src="https://cdn.discordapp.com/attachments/823984320809009222/857319637397274654/logoCanto.png" />
-             <Text>Comunidata</Text>
+            <Formatacao>
+                <Link to="/">
+                    <LogoHeader src="https://cdn.discordapp.com/attachments/823984320809009222/857319637397274654/logoCanto.png" />
+                </Link>
+                <Text>Comunidata</Text>
+            </Formatacao>
             <Div class="nav-right">
-                <Input id = "search" placeholder="search"/>
-                <Link to = {`/produto/${prodName?.id}`}><ButtonPesquisa onClick = {buscarNome}>pesquisar</ButtonPesquisa></Link>
+                <Input id="search" placeholder="search" />
+                <Link to={`/produto/${prodName?.id}`}><ButtonPesquisa onClick={buscarNome}>pesquisar</ButtonPesquisa></Link>
             </Div>
             <Entrar>
                 <Link to="/login" ><button className='entrar'>Entrar</button> </Link>
             </Entrar>
         </HeaderStyle>
-
     )
-
 }
 export default Header;
